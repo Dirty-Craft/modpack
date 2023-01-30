7be6403e199098b7ef8d02d6397ed723e71236e4
parsa shahmaleki <parsampsh@gmail.com>
2023-01-31 00:31:21 +0330

    mod adder script: handle dependencies

--- scripts/add_mod.py
+++ scripts/add_mod.py
@@ -5,17 +5,28 @@ import requests
 import json
 
 
+def is_already_added(mod_id):
+    for item in tools.load_manifest()['files']:
+        if str(item['projectID']) == str(mod_id):
+            return True
+    return False
+
+
 def add_once(slug):
     try:
         result = tools.call_curseforge_api('mods/search?gameId=' + tools.CURSEFORGE_MINECRAFT_GAME_ID + '&classId=' + tools.CURSEFORGE_MINECRAFT_MODS_CLASS_ID + '&slug=' + slug)
         result = result['data'][0]
-    except:
-        print('error: invalid slug "' + slug + '"')
+    except Exception as ex:
+        print('error: failed to load slug "' + slug + '": ' + str(ex))
         return
 
     msg = ('Loading project ID ' + str(result['id']) + ' (' + result['slug'] + ')')
     print('=' * len(msg))
     print(msg)
+
+    if is_already_added(result['id']):
+        print('This mod is already installed. Skipping...')
+        return
 
     item_to_add_to_manifest = {
         "filename": "",
@@ -40,6 +51,18 @@ def add_once(slug):
             break
         except:
             print('error: enter an valid number')
+
+    dependencies = selected_item['dependencies']
+    required_dependencies = []
+    for dep in dependencies:
+        if dep['relationType'] == 3:
+            required_dependencies.append(dep['modId'])
+
+    print('There are ' + str(len(required_dependencies)) + ' required dependencies for this item. Installing them too...')
+
+    for dep in required_dependencies:
+        dep_data = tools.call_curseforge_api('mods/' + str(dep))['data']
+        add_once(dep_data['slug'])
 
     item_to_add_to_manifest['fileID'] = selected_item['id']
     item_to_add_to_manifest['filename'] = selected_item['fileName']
